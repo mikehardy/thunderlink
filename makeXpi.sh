a44225abd7a6f556c97e2c905ad4e0c61910e666
poohsen <poohsen@gmx.de>
2013-09-30 23:56:05 +0200

    made script slightly safer to use

--- makeXpi.sh
+++ makeXpi.sh
@@ -1,2 +1,6 @@
-perl -p -i -e "s/version>.+</version>$1</" install.rdf
-zip -r thunderlink-$1-tb.xpi . -x *.swo *.swp *.xpi *.sh *.git* .gitignore
+if [ -n "$1" ]; then
+    perl -p -i -e "s/version>.+</version>$1</" install.rdf
+    zip -r thunderlink-$1-tb.xpi . -x *.swo *.swp *.xpi *.sh *.git* .gitignore
+else
+    echo "Please give me a version!"
+fi
