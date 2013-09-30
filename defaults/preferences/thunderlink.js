066a27832835688d51449909b13136b1792392d7
poohsen <poohsen@gmx.de>
2013-09-30 21:59:30 +0200

    added defaults for custom strings

---
+++ defaults/preferences/thunderlink.js
@@ -0,0 +1,8 @@
+pref("extensions.thunderlink.custom-tl-string-1-title", "Custom String 1");
+pref("extensions.thunderlink.custom-tl-string-1", "<thunderlink>");
+pref("extensions.thunderlink.custom-tl-string-2-title", "Custom String 2");
+pref("extensions.thunderlink.custom-tl-string-2", "<thunderlink>");
+pref("extensions.thunderlink.custom-tl-string-3-title", "Custom String 3");
+pref("extensions.thunderlink.custom-tl-string-3", "<thunderlink>");
+pref("extensions.thunderlink.custom-tl-string-4-title", "Custom String 4");
+pref("extensions.thunderlink.custom-tl-string-4", "<thunderlink>");
