16a9036c775a2607a64728e19b0061099207ad46
Mike Hardy <github@mikehardy.net>
2018-12-18 18:14:40 -0500

    change versions in and include manifest.json as well

--- makeXpi.sh
+++ makeXpi.sh
@@ -1,6 +1,7 @@
 if [ -n "$1" ]; then
     perl -p -i -e "s/version>.+</version>$1</" install.rdf
-    zip -r thunderlink-$1-tb.xpi chrome/* chrome.manifest components/* CREDITS defaults/* install.rdf LICENSE README.md
+    perl -p -i -e "s/\"version\"\: \".*\"/\"version\"\: \"$1\"/" manifest.json
+    zip -r thunderlink-$1-tb.xpi chrome/* chrome.manifest manifest.json components/* CREDITS defaults/* install.rdf LICENSE README.md
 else
     echo "Please give me a version!"
 fi
