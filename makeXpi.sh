8a0e243e7c08f7994f91b6f5ac4614094ba31864
Mike Hardy <github@mikehardy.net>
2018-12-18 18:25:12 -0500

    don't include manifest.json yet as it triggers one-way change

--- makeXpi.sh
+++ makeXpi.sh
@@ -1,7 +1,8 @@
 if [ -n "$1" ]; then
+    rm thunderlink-$1-tb.xpi
     perl -p -i -e "s/version>.+</version>$1</" install.rdf
     perl -p -i -e "s/\"version\"\: \".*\"/\"version\"\: \"$1\"/" manifest.json
-    zip -r thunderlink-$1-tb.xpi chrome/* chrome.manifest manifest.json components/* CREDITS defaults/* install.rdf LICENSE README.md
+    zip -r thunderlink-$1-tb.xpi chrome/* chrome.manifest components/* CREDITS defaults/* install.rdf LICENSE README.md
 else
     echo "Please give me a version!"
 fi
