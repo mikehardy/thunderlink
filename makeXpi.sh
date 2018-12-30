c234312268ca55c20a2e5ac8f3f6fb25c663f691
Mike Hardy <github@mikehardy.net>
2018-12-30 12:13:38 -0500

    Include the modules directory in the XPI

--- makeXpi.sh
+++ makeXpi.sh
@@ -2,7 +2,7 @@ if [ -n "$1" ]; then
     rm thunderlink-$1-tb.xpi
     perl -p -i -e "s/version>.+</version>$1</" install.rdf
     perl -p -i -e "s/\"version\"\: \".*\"/\"version\"\: \"$1\"/" manifest.json
-    zip -r thunderlink-$1-tb.xpi chrome/* manifest.json chrome.manifest components/* CREDITS defaults/* install.rdf LICENSE README.md
+    zip -r thunderlink-$1-tb.xpi chrome/* manifest.json chrome.manifest components/* CREDITS defaults/* modules/* install.rdf LICENSE README.md
 else
     echo "Please give me a version!"
 fi
