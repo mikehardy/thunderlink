7f41d087f9091b6add6370b7a1b6e723f6d9b3a2
MarioKusek <mario.kusek@fer.hr>
2019-01-03 00:01:00 +0100

    fixed typo in preferences

--- defaults/preferences/thunderlink.js
+++ defaults/preferences/thunderlink.js
@@ -11,7 +11,7 @@
 */
 pref("extensions.thunderlink.custom-tl-string-1-title", "thunderlink");
 pref("extensions.thunderlink.custom-tl-string-1", "<thunderlink>");
-pref("extensions.thunderlink.custom-tl-string-1-slection-delimiter", " / ");
+pref("extensions.thunderlink.custom-tl-string-1-selection-delimiter", " / ");
 pref("extensions.thunderlink.custom-tl-string-1-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-1-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-1-tag", 1);
@@ -20,7 +20,7 @@ pref("extensions.thunderlink.custom-tl-string-1-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-2-title", "local cmd");
 pref("extensions.thunderlink.custom-tl-string-2", "<tbexe> <thunderlink>");
-pref("extensions.thunderlink.custom-tl-string-2-slection-delimiter", "");
+pref("extensions.thunderlink.custom-tl-string-2-selection-delimiter", "");
 pref("extensions.thunderlink.custom-tl-string-2-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-2-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-2-tag", 1);
@@ -29,7 +29,7 @@ pref("extensions.thunderlink.custom-tl-string-2-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-3-title", "RFC 2392");
 pref("extensions.thunderlink.custom-tl-string-3", "mid:<messageid>");
-pref("extensions.thunderlink.custom-tl-string-3-slection-delimiter", " / ");
+pref("extensions.thunderlink.custom-tl-string-3-selection-delimiter", " / ");
 pref("extensions.thunderlink.custom-tl-string-3-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-3-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-3-tag", 1);
@@ -38,7 +38,7 @@ pref("extensions.thunderlink.custom-tl-string-3-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-4-title", "macOS Mail.app");
 pref("extensions.thunderlink.custom-tl-string-4", "message://%3c<messageid>%3e");
-pref("extensions.thunderlink.custom-tl-string-4-slection-delimiter", " / ");
+pref("extensions.thunderlink.custom-tl-string-4-selection-delimiter", " / ");
 pref("extensions.thunderlink.custom-tl-string-4-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-4-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-4-tag", 1);
@@ -47,7 +47,7 @@ pref("extensions.thunderlink.custom-tl-string-4-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-5-title", "clickable subject");
 pref("extensions.thunderlink.custom-tl-string-5", "<A HREF=\"<thunderlink>\"><subject></A>");
-pref("extensions.thunderlink.custom-tl-string-5-slection-delimiter", " / ");
+pref("extensions.thunderlink.custom-tl-string-5-selection-delimiter", " / ");
 pref("extensions.thunderlink.custom-tl-string-5-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-5-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-5-tag", 1);
@@ -56,7 +56,7 @@ pref("extensions.thunderlink.custom-tl-string-5-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-6-title", "clickable subject macOS");
 pref("extensions.thunderlink.custom-tl-string-6", "<A HREF=\"message://%3c<messageid>%3e\"><subject></A>");
-pref("extensions.thunderlink.custom-tl-string-6-slection-delimiter", " / ");
+pref("extensions.thunderlink.custom-tl-string-6-selection-delimiter", " / ");
 pref("extensions.thunderlink.custom-tl-string-6-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-6-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-6-tag", 1);
@@ -65,7 +65,7 @@ pref("extensions.thunderlink.custom-tl-string-6-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-7-title", "Custom String 7");
 pref("extensions.thunderlink.custom-tl-string-7", "<thunderlink>");
-pref("extensions.thunderlink.custom-tl-string-7-slection-delimiter", "");
+pref("extensions.thunderlink.custom-tl-string-7-selection-delimiter", "");
 pref("extensions.thunderlink.custom-tl-string-7-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-7-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-7-tag", 1);
@@ -74,7 +74,7 @@ pref("extensions.thunderlink.custom-tl-string-7-appendtofile-path", "");
 
 pref("extensions.thunderlink.custom-tl-string-8-title", "Custom String 8");
 pref("extensions.thunderlink.custom-tl-string-8", "<thunderlink>");
-pref("extensions.thunderlink.custom-tl-string-8-slection-delimiter", "");
+pref("extensions.thunderlink.custom-tl-string-8-selection-delimiter", "");
 pref("extensions.thunderlink.custom-tl-string-8-clipboard-checkbox", true);
 pref("extensions.thunderlink.custom-tl-string-8-tagcheckbox", false);
 pref("extensions.thunderlink.custom-tl-string-8-tag", 1);
