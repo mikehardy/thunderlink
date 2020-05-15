d2988ded5e0d56b832a337c1c068539f36c0bcd8
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2020-05-15 17:05:43 -0400

    Suppress warning if .xpi does not exist.

--- makeXpi.sh
+++ makeXpi.sh
@@ -1,5 +1,5 @@
 if [ -n "$1" ]; then
-    rm thunderlink-$1-tb.xpi
+    rm -f thunderlink-$1-tb.xpi
     perl -p -i -e "s/version>.+</version>$1</" install.rdf
     perl -p -i -e "s/\"version\"\: \".*\"/\"version\"\: \"$1\"/" manifest.json
     zip -r thunderlink-$1-tb.xpi chrome/* manifest.json chrome.manifest components/* CREDITS defaults/* modules/* install.rdf LICENSE README.md
